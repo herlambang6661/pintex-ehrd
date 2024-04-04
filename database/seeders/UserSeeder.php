d406aa9fbc641812c6ef251130b79be2eb9133ca
herlambang6661 <herlambang6661@gmail.com>
2024-04-04 09:18:18 +0700

    modified:   database/seeders/UserSeeder.php 	deleted:    exl/PSWD.xlsx

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,14 +23,134 @@ class UserSeeder extends Seeder
                 'phone' => '0',
                 'email' => 'IT@pintex.co.id',
             ],
+
             [
-                'name' => 'Puji Nurrenti',
-                'username' => 'puji',
-                'password' => Hash::make('260897'),
-                'role' => 'purchasing',
+                'name' => 'Alvin Tanuwijaya',
+                'username' => 'Alvin',
+                'password' => Hash::make('126912'),
+                'role' => 'admin',
                 'phone' => '0',
-                'email' => 'puji@pintex.co.id',
+                'email' => '',
             ],
+
+            [
+                'name' => 'Nurmayanti',
+                'username' => 'NUR',
+                'password' => Hash::make('alma'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'RAHMAT ADE IRAWAN',
+                'username' => 'ADE',
+                'password' => Hash::make('rizky'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Ireneus Reni',
+                'username' => 'IR',
+                'password' => Hash::make('5421'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Emanuel Tanuwijaya',
+                'username' => 'Ang',
+                'password' => Hash::make('123'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Yohanes Tan',
+                'username' => 'TKC',
+                'password' => Hash::make('all'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Brian Setiadinata',
+                'username' => 'Brian',
+                'password' => Hash::make('123'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Nugroho',
+                'username' => 'Nugroho',
+                'password' => Hash::make('123'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Kartika Dewi',
+                'username' => 'Tika',
+                'password' => Hash::make('7557'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Andjar Boedi Sarwono',
+                'username' => 'Andjar',
+                'password' => Hash::make('123'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Rodo Natorrang Gultom',
+                'username' => 'Rodo',
+                'password' => Hash::make('gultom'),
+                'role' => 'hrd',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Herlambang Yudha',
+                'username' => 'Yudha',
+                'password' => Hash::make('123'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'Felix Jesse',
+                'username' => 'felixjesse',
+                'password' => Hash::make('123'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+            [
+                'name' => 'AHMAD RIZKI',
+                'username' => 'rizki',
+                'password' => Hash::make('123'),
+                'role' => 'admin',
+                'phone' => '0',
+                'email' => '',
+            ],
+
+
         ];
 
         foreach ($user as $key => $value) {
