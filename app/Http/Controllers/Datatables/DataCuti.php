de3e6a7b6f9ea9719826d6ddd13c146070fb70ce
Yudha <herlambang6661@gmail.com>
2024-05-16 13:04:01 +0700

    modified:   app/Http/Controllers/Datatables/DataCuti.php 	modified:   resources/views/products/03_absensi/cuti.blade.php

--- app/Http/Controllers/Datatables/DataCuti.php
+++ app/Http/Controllers/Datatables/DataCuti.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DataCuti extends Controller
 {
@@ -22,13 +23,39 @@ class DataCuti extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-            $data = DB::table('penerimaan_legalitas')
-                ->where('suratjns', 'like', '%perjanjian%')
-                ->where('sacuti', '>', '0')
-                ->where('tglaw', '<=', date('Y-m-d'))
-                ->where('tglak', '>=', date('Y-m-d'))
-                ->orderBy('nama', 'asc')
+            // if (Auth::user()->admin == '1') {
+            //     $unit = 'UNIT 1';
+            //     $data = DB::table('penerimaan_legalitas AS l')
+            //         ->join('penerimaan_karyawan AS k', 'l.userid', '=', 'k.userid')
+            //         ->where('l.suratjns', 'like', '%perjanjian%')
+            //         ->where('k.bagian', 'like', '%' . $unit . '%')
+            //         ->where('l.sacuti', '>', '0')
+            //         ->where('l.tglaw', '<=', date('Y-m-d'))
+            //         ->where('l.tglak', '>=', date('Y-m-d'))
+            //         ->orderBy('l.nama', 'asc')
+            //         ->get();
+            // } elseif (Auth::user()->admin == '2') {
+            //     $unit = 'UNIT 2';
+            //     $data = DB::table('penerimaan_legalitas AS l')
+            //         ->join('penerimaan_karyawan AS k', 'l.userid', '=', 'k.userid')
+            //         ->where('l.suratjns', 'like', '%perjanjian%')
+            //         ->where('k.bagian', 'like', '%' . $unit . '%')
+            //         ->where('l.sacuti', '>', '0')
+            //         ->where('l.tglaw', '<=', date('Y-m-d'))
+            //         ->where('l.tglak', '>=', date('Y-m-d'))
+            //         ->orderBy('l.nama', 'asc')
+            //         ->get();
+            // }
+
+            $data = DB::table('penerimaan_legalitas AS l')
+                ->join('penerimaan_karyawan AS k', 'l.userid', '=', 'k.userid')
+                ->where('l.suratjns', 'like', '%perjanjian%')
+                ->where('l.sacuti', '>', '0')
+                ->where('l.tglaw', '<=', date('Y-m-d'))
+                ->where('l.tglak', '>=', date('Y-m-d'))
+                ->orderBy('l.nama', 'asc')
                 ->get();
+
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('cutiTerpakai', function ($row) {
