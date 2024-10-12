1ded182a9356e282037e0466ff604e5c05a1f42b
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-10-12 10:04:42 +0700

    destroyLowongan

--- resources/views/products/02_penerimaan/lowongan.blade.php
+++ resources/views/products/02_penerimaan/lowongan.blade.php
@@ -126,20 +126,31 @@
                             <div class="card h-100">
                                 <div class="card-header d-flex justify-content-between align-items-center">
                                     <h2 class="mb-0">{{ $l->posisi }}</h2>
-                                    <form action="{{ route('updateRelease', $l->id) }}" method="POST">
-                                        @csrf
-                                        <button class="btn" type="submit">
-                                            @if ($l->release == 1)
-                                                Active <span class="badge bg-blue ms-2"><i
-                                                        class="fa-solid fa-check"></i></span>
-                                            @else
-                                                InActive <span class="badge bg-red ms-2"><i
-                                                        class="fa-solid fa-xmark"></i></span>
-                                            @endif
-                                        </button>
-                                    </form>
-
+                                    <div class="d-flex align-items-center">
+                                        <form action="{{ route('updateRelease', $l->id) }}" method="POST" class="me-2">
+                                            @csrf
+                                            <button class="btn" type="submit">
+                                                @if ($l->release == 1)
+                                                    Active <span class="badge bg-blue ms-2"><i
+                                                            class="fa-solid fa-check"></i></span>
+                                                @else
+                                                    InActive <span class="badge bg-red ms-2"><i
+                                                            class="fa-solid fa-xmark"></i></span>
+                                                @endif
+                                            </button>
+                                        </form>
+                                        <form id="delete-form-{{ $l->id }}"
+                                            action="{{ route('lowongan.destroy', $l->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger btn-sm" type="button" data-bs-toggle="modal"
+                                                data-bs-target="#confirmDeleteModal-{{ $l->id }}">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
+
                                 <div class="card-body d-flex justify-content-between align-items-center">
                                     <div>
                                         <h4 class="mb-2">Pendidikan: {{ $l->pendidikan }}</h4>
@@ -227,6 +238,12 @@
                                             <div class="mb-5 p-4">
                                                 {!! $l->deskripsi !!}
                                             </div>
+                                            <div
+                                                style="max-width: 100px; max-height: 100px; margin-left: 15px; margin-bottom: 15px;">
+                                                <img src="{{ Storage::url($l->image) }}" class="img-fluid rounded"
+                                                    style="object-fit: cover; width: 100%; height: 100%;"
+                                                    alt="Job Image 1">
+                                            </div>
                                             @if ($l->unlimited != 1)
                                                 <div class="mb-0 d-flex justify-content-between">
                                                     <div>
@@ -238,6 +255,7 @@
                                                     </div>
                                                 </div>
                                             @endif
+
                                         </div>
                                     </div>
                                 </div>
@@ -249,6 +267,48 @@
                         <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
                             Cancel
                         </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- modal destroy --}}
+        <!-- Modal -->
+        <div class="modal" id="confirmDeleteModal-{{ $l->id }}" tabindex="-1"
+            aria-labelledby="confirmDeleteModalLabel-{{ $l->id }}" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="modal-status bg-danger"></div>
+                    <div class="modal-body text-center py-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24"
+                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 9v2m0 4v.01" />
+                            <path
+                                d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75" />
+                        </svg>
+                        <h3>Apakah Anda Yakin Ingin Mengapus Lowongan ini {{ $l->posisi }}?</h3>
+                        <div class="text-secondary">Anda akan menghapus posisi ini dengan permanen.
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="w-100">
+                            <div class="row">
+                                <div class="col">
+                                    <button class="btn w-100" data-bs-dismiss="modal">
+                                        Batal
+                                    </button>
+                                </div>
+                                <div class="col">
+                                    <button class="btn btn-danger w-100"
+                                        onclick="document.getElementById('delete-form-{{ $l->id }}').submit()">
+                                        Hapus
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -291,4 +351,5 @@
             }
         });
     </script>
+    {{-- sweetalert destroy --}}
 @endsection
